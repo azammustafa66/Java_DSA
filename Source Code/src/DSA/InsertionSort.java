8f9ef2f8b047b530350e5e044993a5de4e916f9c
azammustafa66 <azammustafa66@gmail.com>
2022-12-24 11:33:16 +0530

    Java

--- Source Code/src/DSA/InsertionSort.java
+++ Source Code/src/DSA/InsertionSort.java
@@ -1,26 +1,28 @@
 package DSA;
-
 import java.util.Arrays;
 
 public class InsertionSort {
+    // With each pass array is getting sorted j times
     public static void main(String[] args) {
         int[] arr = {5, 3, 2, 1, 4};
-        BubbleSort(arr);
+        Insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void BubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < arr.length - i; j++) {
+    static void Insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
                 if (arr[j - 1] > arr[j])
                     Swap(arr, j - 1, j);
+                else
+                    break;
             }
         }
     }
 
-    public static void Swap(int[] arr, int first, int last) {
+    static void Swap(int[] arr, int first, int second) {
         int temp = arr[first];
-        arr[first] = arr[last];
-        arr[last] = temp;
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
