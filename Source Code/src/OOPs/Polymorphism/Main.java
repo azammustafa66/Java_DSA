489a260c987a49f4c80c689812cb4627530d7bb4
azammustafa66 <azammustafa66@gmail.com>
2023-01-30 21:44:33 +0530

    Java

---
+++ Source Code/src/OOPs/Polymorphism/Main.java
@@ -0,0 +1,11 @@
+package OOPs.Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        Shapes circle = new Circle();
+        Square square = new Square();
+
+        circle.area();
+    }
+}
---
+++ Source Code/src/OOPs/Polymorphism/Square.java
@@ -0,0 +1,7 @@
+package OOPs.Polymorphism;
+
+public class Square extends Shapes{
+    void area() {
+        System.out.println("Area is square of sides");
+    }
+}
